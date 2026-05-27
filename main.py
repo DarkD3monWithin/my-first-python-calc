009c29d6206fa4e37a730d5f138cf143f1fd014a
DarkD3monWithin <brycegerman21@gmail.com>
2026-05-26 21:25:08 -0600

    Added Square Roots

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
+import math
 
 while True:
     try:
         a = int(input('Enter first number: '))
-        b = int(input('Enter second number: '))
+        b = int(input('Enter second number (or just enter 0 for square_root): '))
         c = input('Enter operation: ')
 
         if c == '+':
@@ -15,14 +16,16 @@ while True:
                 answer = a / b
             else:
                 print('Error: Cannot divide by 0!')
-                break
+                continue
         elif c == '*':
             answer = a * b
         elif c == '**':
             answer = a ** b
+        elif c == 'square_root':
+            answer = math.sqrt(a)
         else:
             print('Invalid Syntax!')
-            break
+            continue
         print("Calculating...")
         time.sleep(1)
         print(f'Result: {answer}')
